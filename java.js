a0c6807e2b96a3669d49a4c0b90dceedbb011d66
thankyou123456789 <69336071+thankyou123456789@users.noreply.github.com>
2020-12-22 11:14:28 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,31 @@
+function Function3(){
+    let x = document.getElementById("myBtn").value;
+    // document.getElementById("images").src="green.png";
+    document.getElementByld("images").src = x ;
+}
+
+function Function4() {
+    let x = document.getElementById("myBtn4").value;
+    document.getElementById("images").src = blue.jpg ;
+    // document.getElementById("images").src = x ;
+}
+function Function5() {
+    let x = document.getElementById("myBtn5").value;
+    // document.getElementById("images").src = x ;
+    document.getElementById("images").src = x ;
+}
+function Function6() {
+    document.getElementById("text").style.color="green";
+}
+function Function7() {
+    document.getElementById("text").style.color="blue";
+}
+function Function8() {
+    document.getElementById("text").style.color="brown";
+}
+function function9() {
+    document.getElementById("hideDisplay").style.display="none";
+}
+function function10() {
+    document.getElementById("hideDisplay").style.display="block";
+}
